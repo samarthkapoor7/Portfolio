b2dc05082d1cbf6311ebfb5e896ee80d58129981
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-08-06 19:17:48 +0530

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
             initial={{opacity: 0, x: 100}}
             transition={{duration: 1}}
             className="my-4">
-              {CONTACT.phoneNo}
+            
             </motion.p>
             <a href="mailto:kapoorsamarth7@gmail.com" className="border-b">
             {CONTACT.email}</a>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/mecropped.jpg"
+import profilePic from "../assets/me.png"
 import { delay, motion } from "framer-motion";
+import { HiDownload } from "react-icons/hi";
 
 
 const container = (delay) => ({
@@ -36,6 +37,16 @@ const Hero = () => {
             className="my-2 max-w-xl py-6 font-light tracking-tighter">
               {HERO_CONTENT}
             </motion.p>
+            <motion.div variants={container(1.5)}
+            initial="hidden"
+            animate="visible">
+            <a initial="hidden"
+            animate="visible"
+            className="group mr-2 mb-7 rounded-full px-7 py-3 flex text-sm items-center gap-2 font-medium text-gradient-to-r from-pink-300 via-slate-500
+            to-purple-500 border border-purple-600 focus:scale-100 hover:scale-100 hover:bg-gray-800 active:scale-105 transition cursor-pointer" href="src\assets\Samarth_cv.pdf" download>
+              Download CV <HiDownload  className="opacity-60 group-hover:translate-y-1 transition" />
+            </a>
+            </motion.div>
          </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
