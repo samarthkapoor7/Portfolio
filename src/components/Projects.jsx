cae4bd1094a522499c600d5cc67aa840d7dbbefd
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-11-30 00:20:47 +0530

    added links to Project images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,18 +18,22 @@ const Projects = () => {
             initial={{opacity: 0, x: -100}}
             transition={{duration: 1}}
             className="w-full lg:w-1/4">
-              <img src={project.image} 
-              width={150} 
-              height={150} 
-              alt={project.title} 
-              className="mb-6 rounded"/>
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                <img src={project.image} 
+                width={150} 
+                height={150} 
+                alt={project.title} 
+                className="mb-6 rounded hover:opacity-90 transition-opacity duration-300"/>
+              </a>
             </motion.div>
             <motion.div
             whileInView={{opacity: 1, x: 0}}
             initial={{opacity: 0, x: 100}}
             transition={{duration: 1}}
             className="w-full max-w-xl lg:w-3/4">
-               <h6 className="mb-2 font-semibold">{project.title}</h6>
+               <a href={project.link} target="_blank" rel="noopener noreferrer" className="hover:underline">
+                <h6 className="mb-2 font-semibold">{project.title}</h6>
+               </a>
                <p className="mb-4 text-neutral-400">{project.description}</p>
                {project.technologies.map((tech, index) => (
                  <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900">
